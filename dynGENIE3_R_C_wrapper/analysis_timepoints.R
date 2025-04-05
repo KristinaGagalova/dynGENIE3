b80d003115223356e47c51f5984d2059609ec85c
Kristina Gagalova <kristina.gagalova@curtin.edu.au>
2025-04-06 07:24:23 +0800

    working version analysis_timepoints.R

--- dynGENIE3_R_C_wrapper/analysis_timepoints.R
+++ dynGENIE3_R_C_wrapper/analysis_timepoints.R
@@ -1,6 +1,13 @@
+#!/usr/bin/env Rscript
+
 # Load data from df and prepare for output
 # dynGENIE3
 # Polished by Kristina Gagalova
+
+library("doRNG")
+library("doParallel")
+
+source("dynGENIE3.R")
 
 # Read the raw counts table
 data <- read.table(gzfile("../RawCountsStar-Aka.tsv.gz"), sep="\t", header=TRUE, row.names=1)
@@ -45,10 +52,9 @@ for (treatment in treatments) {
   write.table(avg_expr_df, file=paste0("averaged_expression_", treatment, ".txt"), sep="\t", quote=FALSE, row.names=FALSE)
 }
 
-
 # load data with function from package
-TS1 <- read.expr.matrix("averaged_expression_Pet.txt", form="rows.are.samples")
-TS2 <- read.expr.matrix("averaged_expression_Tox8F.txt", form="rows.are.samples")
+TS1 <- read.expr.matrix("./averaged_expression_Pet.txt", form="rows.are.samples")
+TS2 <- read.expr.matrix("./averaged_expression_Tox8F.txt", form="rows.are.samples")
 
 regulators <- read.csv(
      "../DEseq_AkaPval1.in",
@@ -56,18 +62,26 @@ regulators <- read.csv(
    )
 
 # Find intersection with regulator genes
-common_genes <- Reduce(intersect, list(colnames(TS1), colnames(TS2), regulators))
+common_genes <- Reduce(intersect, list(row.names(TS1), row.names(TS2), regulators$V1))
 common_genes <- unlist(common_genes)
 
+time.points <- list(unname(TS1[1, ]), unname(TS2[1, ]))
+
+# Find intersection with regulator genes
+common_genes <- Reduce(intersect, list(row.names(TS1), row.names(TS2), regulators$V1))
+common_genes <- unlist(common_genes)
+
+time.points <- list(unname(TS1[1, ]), unname(TS2[1, ]))
+
 # Subset dataframes to include only intersected genes
-TS1 <- TS1[, common_genes, drop=FALSE]
-TS2 <- TS2[, common_genes, drop=FALSE]
+TS1 <- TS1[common_genes, ,drop=FALSE]
+TS2 <- TS2[common_genes, ,drop=FALSE]
 
-
-# prepare input
-time.points <- list(unname(TS1[,1]), unname(TS2[,1]))
+#TS.data <- list(TS1[2:nrow(TS1),], TS2[2:nrow(TS2),])
+TS.data <- list(TS1, TS2)
 
 # dynGENIE3 run
-dynGENIE3_results <- dynGENIE3(TS.data = TS.data, 
-                                time.points = time.points, 
-                                regulators = common_genes)
+dynGENIE3_results <- dynGENIE3(TS.data = TS.data,
+                              time.points = time.points,
+                              regulators = common_genes,
+                               ncores=6)
